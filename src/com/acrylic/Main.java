d38e0c6f303fc73f2b3f05f48be7a0540f49555d
Acrylic125 <acrylic125email@gmail.com>
2021-02-06 12:19:29 +0800

    Initial commit

---
+++ src/com/acrylic/Main.java
@@ -0,0 +1,25 @@
+package com.acrylic;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("controllers/sample.fxml"));
+        primaryStage.setTitle("School Utilities");
+        var scene = new Scene(root, 700, 430);
+        scene.getStylesheets().add(getClass().getResource("resources/DarkTheme.css").toString());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/acrylic/controllers/Controller.java
@@ -0,0 +1,11 @@
+package com.acrylic.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+
+
+}
